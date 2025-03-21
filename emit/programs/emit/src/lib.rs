69a35d6629471861d0bdb300afc2947b0fb6506b
0xE <813466251@qq.com>
2025-03-21 13:08:04 +0800

    Article 13

---
+++ emit/programs/emit/src/lib.rs
@@ -0,0 +1,28 @@
+use anchor_lang::prelude::*;
+
+declare_id!("4cP6BBgaRn5Z7kYkrNvipxKQNk2dVZDtHGC3w16QLiuE");
+
+#[program]
+pub mod emit {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        emit!(MyEvent { value: 42 });
+        emit!(MySecondEvent { value: 3, message: "hello world".to_string() });
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
+
+#[event]
+pub struct MyEvent {
+    pub value: u64,
+}
+
+#[event]
+pub struct MySecondEvent {
+    pub value: u64,
+    pub message: String,
+}
---
+++ emit/tests/emit.ts
@@ -0,0 +1,26 @@
+import * as anchor from "@coral-xyz/anchor";
+import { BorshCoder, EventParser, Program } from "@coral-xyz/anchor";
+import { Emit } from "../target/types/emit";
+
+describe("emit", () => {
+  anchor.setProvider(anchor.AnchorProvider.env());
+
+  const program = anchor.workspace.Emit as Program<Emit>;
+
+  it("Is initialized!", async () => {
+    const listenerMyEvent = program.addEventListener('MyEvent', (event, slot) => {
+      console.log(`slot ${slot} event value ${event.value}`);
+    });
+
+    const listenerMySecondEvent = program.addEventListener('MySecondEvent', (event, slot) => {
+      console.log(`slot ${slot} event value ${event.value} event message ${event.message}`);
+    });
+
+    await program.methods.initialize().rpc();
+
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+
+    program.removeEventListener(listenerMyEvent);
+    program.removeEventListener(listenerMySecondEvent);
+  });
+});
